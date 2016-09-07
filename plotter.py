cfa516a6c2702576f23ef982af5886ffb23deb8a
Jan Pawellek <pawellek@stud.uni-heidelberg.de>
2016-09-07 16:48:04 +0200

    Modified plotter.py for batch processing

--- plotter.py
+++ plotter.py
@@ -5,45 +5,42 @@ import os
 import csv
 
 def main():
-    if len(sys.argv) != 2:
-        print 'Usage: ./plotter.py <resultfile>'
+    if len(sys.argv) < 2:
+        print 'Usage: ./plotter.py <resultfile, resultfile, ...>'
         return
-    resultfn = sys.argv[1]
+    resultfns = sys.argv[1:]
 
-    print 'Welcome to plotter.py'
-    print ''
-    print 'Data file: %s' % resultfn
-    print ''
+    print "file, total, avgsec, avgsrccpu, avgsrcmem, avgtgtcpu, avgtgtmem"
+    for resultfn in resultfns:
+        resultf = open(resultfn)
+        reader = csv.DictReader(resultf)
+        total = 0
+        avgsec = .0
+        avgsrccpu = .0
+        avgsrcmem = .0
+        avgtgtcpu = .0
+        avgtgtmem = .0
+        for row in reader:
+            total += 1
+            avgsec += float(row['seconds'])
+            avgsrccpu += float(row['sourcecpu'])
+            avgsrcmem += float(row['sourcemem'])
+            avgtgtcpu += float(row['targetcpu'])
+            avgtgtmem += float(row['targetmem'])
+        avgsec /= total
+        avgsrccpu /= total
+        avgsrcmem /= total
+        avgtgtcpu /= total
+        avgtgtmem /= total
+        # print 'Total lines: %i' % total
+        # print 'Avg seconds: %.2f' % avgsec
+        # print 'Avg src cpu: %.2f' % avgsrccpu
+        # print 'Avg src mem: %.2f' % avgsrcmem
+        # print 'Avg tgt cpu: %.2f' % avgtgtcpu
+        # print 'Avg tgt mem: %.2f' % avgtgtmem
+        print "%s, %i, %.2f, %.2f, %.2f, %.2f, %.2f" % (resultfn, total, avgsec, avgsrccpu, avgsrcmem, avgtgtcpu, avgtgtmem)
 
-    # 1. Read CSV file
-    resultf = open(resultfn)
-    reader = csv.DictReader(resultf)
-    total = 0
-    avgsec = .0
-    avgsrccpu = .0
-    avgsrcmem = .0
-    avgtgtcpu = .0
-    avgtgtmem = .0
-    for row in reader:
-        total += 1
-        avgsec += float(row['seconds'])
-        avgsrccpu += float(row['sourcecpu'])
-        avgsrcmem += float(row['sourcemem'])
-        avgtgtcpu += float(row['targetcpu'])
-        avgtgtmem += float(row['targetmem'])
-    avgsec /= total
-    avgsrccpu /= total
-    avgsrcmem /= total
-    avgtgtcpu /= total
-    avgtgtmem /= total
-    print 'Total lines: %i' % total
-    print 'Avg seconds: %.2f' % avgsec
-    print 'Avg src cpu: %.2f' % avgsrccpu
-    print 'Avg src mem: %.2f' % avgsrcmem
-    print 'Avg tgt cpu: %.2f' % avgtgtcpu
-    print 'Avg tgt mem: %.2f' % avgtgtmem
-
-    resultf.close()
+        resultf.close()
 
 if __name__ == '__main__':
     main()
