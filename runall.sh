7ba337f6728ca63310fa68be82a79c6ca5e0a49a
Jan Pawellek <pawellek@stud.uni-heidelberg.de>
2016-09-22 13:31:27 +0200

    Added results src2-tgt1 with bro

--- runall.sh
+++ runall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./runs/par-bro-iter.sh
-./runs/par-nobro-iter.sh
-./runs/seq-bro-iter.sh
-./runs/seq-nobro-iter.sh
+#./runs/par-bro-iter.sh
+#./runs/par-nobro-iter.sh
+#./runs/seq-bro-iter.sh
+#./runs/seq-nobro-iter.sh
 ./runs/src2-tgt1/par-bro-iter.sh
 ./runs/src2-tgt1/par-nobro-iter.sh
 ./runs/src2-tgt1/seq-bro-iter.sh
